7eec2f2ce102ff6377ba9c6563cc08e41d51aff2
juanrv01 <juan.e.rodriguez@correounivalle.edu.co>
2024-12-11 22:40:24 -0500

    Cambio serializers products

--- products/serializers.py
+++ products/serializers.py
@@ -12,37 +12,30 @@ class CategorySerializer(serializers.ModelSerializer):
         fields='__all__'
 
 class ImageSerializer(serializers.ModelSerializer):
-    
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
-        model=Image
-        fields= ['image']
+        model = Image
+        fields = ['image', 'image_url']
+
+    def get_image_url(self, obj):
+        return obj.image.url
+
         
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     images = ImageSerializer(many=True)
     is_in_cart = serializers.SerializerMethodField()
-    
+
     class Meta:
-        model=Product
+        model = Product
         fields = ['id', 'name', 'description', 'price', 'quantity', 'category', 'images', 'is_in_cart']
-        
 
     def get_is_in_cart(self, obj):
-        print('sdfsfsdfd')
         user = self.context['request'].user
         if isinstance(user, AnonymousUser):
             return False
         try:
             return user.cart.products.filter(id=obj.id).exists()
         except CustomUser.cart.RelatedObjectDoesNotExist:
-            return False
-
-
-    def get_is_in_wishlist(self, obj):
-        user = self.context['request'].user
-        if isinstance(user, AnonymousUser):
-            return False
-        try:
-            return user.wishlist.product.filter(id=obj.id).exists()
-        except CustomUser.wishlist.RelatedObjectDoesNotExist:
             return False
